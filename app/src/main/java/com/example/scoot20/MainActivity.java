4a41febcf458e13096dfd73569458952485ae2a6
Aniq <eatcookiesbeast@gmail.com>
2023-01-14 21:50:10 +0800

    commit

--- app/src/main/java/com/example/scoot20/MainActivity.java
+++ app/src/main/java/com/example/scoot20/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.scoot20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.activities:
+                        startActivity(new Intent(getApplicationContext(),ViewBookingActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        return true;
+                    case R.id.ewallet:
+                        startActivity(new Intent(getApplicationContext(),EWallet.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.message:
+                        startActivity(new Intent(getApplicationContext(),Message.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(),Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/scoot20/ViewBookingActivity.java
+++ app/src/main/java/com/example/scoot20/ViewBookingActivity.java
@@ -1,8 +1,13 @@
 package com.example.scoot20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ViewBookingActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class ViewBookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_booking);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.activities);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.activities:
+                        return true;
+                    case R.id.ewallet:
+                        startActivity(new Intent(getApplicationContext(),EWallet.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.message:
+                        startActivity(new Intent(getApplicationContext(),Message.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(),Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
